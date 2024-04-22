91b47f25db8809f55d282c5651fa1854fafaa7af
PreethamVenkatesh <30716236+PreethamVenkatesh@users.noreply.github.com>
2024-04-22 21:34:58 +0100

    manager approval for signed up user added

--- src/BlueTerrain/Login.java
+++ src/BlueTerrain/Login.java
@@ -25,11 +25,10 @@ public class Login {
     private static String SIGN_UP = "Don't have an account? Sign up";
     private static String ENTER_USERNAME = "Enter your username";
     private static String INVALID_USERNAME = "Invalid username.";
-    private static String FAILED_AUTHENTICATE = "Error: Failed to authenticate user";
     private static String CUSTOMER = "Customer";
     private static String STAFF = "Staff";
 
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) {        
         VBox root = Functions.createRootVBox();
 
         HBox loginTypeBox = new HBox(10);
@@ -37,7 +36,8 @@ public class Login {
         Label loginTypeLabel = new Label(LOGIN_TYPE);
         loginTypeLabel.setFont(new Font(15));
         loginTypeLabel.setStyle("-fx-text-fill: black;");
-        ChoiceBox<String> loginTypeChoiceBox = new ChoiceBox<>(FXCollections.observableArrayList("Customer", "Staff"));
+        ChoiceBox<String> loginTypeChoiceBox = new 
+                ChoiceBox<>(FXCollections.observableArrayList(CUSTOMER, STAFF));
         loginTypeChoiceBox.getSelectionModel().selectFirst();
         loginTypeChoiceBox.setStyle("-fx-text-fill: black;");
         loginTypeBox.getChildren().addAll(loginTypeLabel, loginTypeChoiceBox);
@@ -48,25 +48,29 @@ public class Login {
         Hyperlink signUpLink = new Hyperlink(SIGN_UP);
         signUpLink.setOnAction(e -> {
             Signup signUpPage = new Signup();
-            signUpPage.start(primaryStage, "Customer");
+            signUpPage.start(primaryStage, CUSTOMER);
         });
 
         Text errorMessage = new Text();
         errorMessage.setFill(Color.RED);
 
-        root.getChildren().addAll(Functions.welcomePane(), loginTypeBox,
-                usernameBox, loginButton, signUpLink, errorMessage);
-        Functions.setupAndShowScene(primaryStage, root, 800, 600);
-
+        root.getChildren().addAll(Functions.welcomePane(), loginTypeBox, 
+                    usernameBox, loginButton, signUpLink, errorMessage);
+        Functions.setupAndShowScene(primaryStage, root, 800, 600); 
+        
         loginButton.setOnAction(e -> {
             TextField usernameField = (TextField) usernameBox.getChildren().get(1);
             String username = usernameField.getText();
             String loginType = loginTypeChoiceBox.getValue();
 
-            if (authenticate(username, loginType)) {
+            String[] userDetails = authenticate(username, loginType);
+            if (userDetails != null) {
+                String firstName = userDetails[0];
+                String lastName = userDetails[1];
+                String profileType = userDetails[2];
                 if (loginType.equals(STAFF)) { 
                     Restaurant restaurant = new Restaurant();
-                    restaurant.start(primaryStage);
+                    restaurant.start(primaryStage, firstName, lastName, profileType);
                 } else if (loginType.equals(CUSTOMER)) {
                     Bookings bookings = new Bookings();
                     bookings.start(primaryStage);
@@ -76,24 +80,36 @@ public class Login {
             } else {
                 errorMessage.setText(INVALID_USERNAME);
             }
-
         });
     }
 
-    private boolean authenticate(String username, String loginType) {
-        String tableName = (loginType.equals("Customer")) ? "customers" : "staffs";
+    private String[] authenticate(String username, String loginType) {
+        String tableName = (loginType.equals(CUSTOMER)) ? "customers" : "staffs";
         String query = "SELECT * FROM " + tableName + " WHERE first_name = ?";
-        try (Connection connection = Functions.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+        
+        try (Connection connection = Functions.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
             preparedStatement.setString(1, username);
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                return resultSet.next();
-
+                if (resultSet.next()) {
+                    String firstName = resultSet.getString("first_name");
+                    String lastName = resultSet.getString("last_name");
+                    String profileType = resultSet.getString("profile_type");
+                    if (loginType.equals(STAFF)) {
+                        boolean approved = resultSet.getBoolean("isApproved");
+                        return new String[]{firstName, lastName, profileType, String.valueOf(approved)};
+                    }
+                    return new String[]{firstName, lastName, profileType};
+                } else {
+                    return null; // Username does not exist in the database
+                } 
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
-            System.err.println(FAILED_AUTHENTICATE);
-            return false;
-
+            System.err.println("Error: Failed to authenticate user - " + ex.getMessage()); // Print the exception message
+            return null; 
         }
     }
 }
+
+
