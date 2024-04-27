8bfd480523a07e54eb2e283b7843549f2ee24183
PreethamVenkatesh <30716236+PreethamVenkatesh@users.noreply.github.com>
2024-04-27 12:53:57 +0100

    root generic method called

--- src/BlueTerrain/Staff.java
+++ src/BlueTerrain/Staff.java
@@ -25,10 +25,7 @@ public class Staff {
     private static String STAFF_QUERY = "SELECT CONCAT(first_name, ' ', last_name) AS result_column FROM staffs WHERE profile_type = ?";
     
     public static void showStaffPopup(Stage primaryStage) {
-
-        VBox root = Functions.createRootVBox();
-        root.setAlignment(Pos.TOP_CENTER);
-        root.setBackground(Functions.backGroundImage("/BlueTerrain/Images/BT_Common.jpeg"));
+        VBox root = Functions.commonHeader("/BlueTerrain/Images/BT_Common.jpeg");
 
         Button managerButton = Functions.createButtonMenu("MANAGERS", Color.LAVENDER);
         Button chefButton = Functions.createButtonMenu("CHEFS", Color.LAVENDER);
@@ -47,12 +44,7 @@ public class Staff {
         buttonsBox.setAlignment(Pos.CENTER);
         buttonsBox.getChildren().addAll(leftBox, rightBox);
 
-        Button closeButton = new Button("Close");
-        closeButton.setAlignment(Pos.BOTTOM_RIGHT);
-        closeButton.setOnAction(e -> {
-            Restaurant restaurant = new Restaurant();
-            restaurant.start(primaryStage, Menu.getFirstName(), Menu.getLastName(), Menu.getProfileType());
-        });
+        Button closeButton = Functions.closeButton(primaryStage);
 
         root.getChildren().addAll(Functions.welcomePane(), buttonsBox, closeButton);
         Functions.setupAndShowScene(primaryStage, root);
