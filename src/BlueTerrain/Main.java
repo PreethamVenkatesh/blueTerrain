d2f6a7c36c1918748e019df71b91e47659cfc241
ManasaR05 <153731907+ManasaR05@users.noreply.github.com>
2024-05-03 00:13:30 +0100

    Update Main.java
    java comments updated

--- src/BlueTerrain/Main.java
+++ src/BlueTerrain/Main.java
@@ -5,7 +5,7 @@ import javafx.stage.Stage;
 
 /**
  * The Main class serves as the entry point of the BlueTerrain application.
- * @author Preetham
+ * @author Preetham Venkatesh
  */
 public class Main extends Application {
    /**
@@ -15,13 +15,14 @@ public class Main extends Application {
      * @param args The command line arguments passed to the application.
      */ 
     public static void main(String[] args) {
-        launch(args);
+        launch(args); // Launches the JavaFX application by calling the launch method.
     }
 
     @Override
     public void start(Stage primaryStage) {
-
+   // Initializes a new instance of the Login class.
         Login login = new Login();
+   // Starts the login process by calling the start method of the Login instance, passing the primary stage.
         login.start(primaryStage);
         
     }
