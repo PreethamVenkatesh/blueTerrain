536d7d8d7691bac1e3a534e8eccc4e6e20d728fa
Preetham Venkatesh <preetham.venkyy@gmail.com>
2024-05-02 12:24:14 +0100

    Orders bug fix

--- src/BlueTerrain/ChefSpecial.java
+++ src/BlueTerrain/ChefSpecial.java
@@ -6,7 +6,6 @@ import java.sql.SQLException;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
